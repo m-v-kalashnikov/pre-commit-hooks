af28d47e38af3148772ce9d64c5e92fd82162e10
Michael Kalashnikov <kalashnikovsystem@gmail.com>
2022-04-19 10:47:40 +0300

    init: test

--- go/vet.sh
+++ go/vet.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
-##!/usr/bin/env bash
-#
-#set -o errexit   # abort on nonzero exitstatus
-#set -o nounset   # abort on unbound variable
-#set -o pipefail  # don't hide errors within pipes
-#
-#main() {
-#  for dir in $(echo "$@"|xargs -n1 dirname|sort -u); do
-#    go vet ./"$dir"
-#  done
-#}
-#
-#main "$@"
 
-for DIR in $(echo "$@"|xargs -n1 dirname|sort -u); do
-    go vet ./"$DIR"
-done
+set -o errexit   # abort on nonzero exitstatus
+set -o nounset   # abort on unbound variable
+set -o pipefail  # don't hide errors within pipes
+
+main() {
+  for dir in $(echo "$@"|xargs -n1 dirname|sort -u); do
+    go vet ./"$dir"
+  done
+}
+
+main "$@"
